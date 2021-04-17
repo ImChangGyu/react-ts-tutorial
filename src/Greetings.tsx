9d8c1b8d8ac271afdb69ec9ed317ea6cb24bbf51
임창규 <audu1217@naver.com>
2021-04-17 15:32:58 +0900

    [update]Greeting.tsx

--- src/Greetings.tsx
+++ src/Greetings.tsx
@@ -2,10 +2,21 @@ import React from "react";
 
 type GreetingProps = {
     name: string;
+    mark: string;
+    optional?: string;
 };
 
-const Greetings: React.FC<GreetingProps> = ({ name }) => {
-    return <div>Hello, {name}</div>;
+const Greetings = ({ name, mark, optional }: GreetingProps) => {
+    return (
+        <div>
+            Hello, {name} {mark}
+            {optional && <p>{optional}</p>}
+        </div>
+    );
+};
+
+Greetings.defaultProps = {
+    mark: "!",
 };
 
 export default Greetings;
