6037ac11c4e10e42dcbc467b5ee0922a5f718728
임창규 <audu1217@naver.com>
2021-04-19 13:51:08 +0900

    [add]Greeting.tsx

---
+++ src/components/Greetings.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+type GreetingProps = {
+    name: string;
+    mark: string;
+    optional?: string;
+};
+
+const Greetings = ({ name, mark, optional }: GreetingProps) => {
+    return (
+        <div>
+            Hello, {name} {mark}
+            {optional && <p>{optional}</p>}
+        </div>
+    );
+};
+
+Greetings.defaultProps = {
+    mark: "!",
+};
+
+export default Greetings;
